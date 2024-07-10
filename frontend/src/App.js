e926f8feac3862e8074fe51d206976ded624150c
BushraArif90 <125812447+BushraArif90@users.noreply.github.com>
2024-07-10 15:08:53 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,7 +49,7 @@ function App() {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const { data } = await axios.get(`/api/products/categories`);
+        const { data } = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/products/categories`);
         setCategories(data);
       } catch (err) {
         toast.error(getError(err));
